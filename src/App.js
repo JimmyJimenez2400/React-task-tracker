eb0e3eaf56c410df44020a52fb1793b3bfec77a7
Jimmy Jimenez <jimmyjimenez14@yahoo.com>
2021-06-02 22:14:04 -0400

    First ever commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const[tasks, setTasks] = useState([ 
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30PM',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'School Appointment',
+        day: 'Feb 6th at 2:30PM',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Doc Appointment',
+        day: 'Feb 5th at 2:30PM',
+        reminder: false,
+    
+    },
+    ])
+  return(
+    <div className='container'>
+      <Header />
+      <Tasks tasks={tasks} />
     </div>
-  );
+  )
 }
 
+Header.defaultProps ={
+  title: 'Task Tracker',
+}
 export default App;
